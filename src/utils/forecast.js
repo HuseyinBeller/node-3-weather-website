c766a225a5d280116b8c657fce7b5c5a0e3bd625
Hüseyin Beller <bellerhuseyin@gmail.com>
2021-02-10 22:51:37 +0100

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('request')
+
+
+const forecast =(latidude, longitude, callback) => {
+  const url = 'http://api.weatherstack.com/current?access_key=498a3b463c930b04433d70fa18768d50&query=' + latidude + ',' + longitude + '&units=f'   
+
+   request({ url, json: true}, (error, { body }) => {
+     if (error) {
+        callback('Unable to connect to weather service!', undefined)
+     }  else if (body.error) {
+        callback('Unable to find location!', undefined)
+     }  else {
+        callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degress out.')
+     }
+   })
+}
+
+
+    module.exports = forecast
+
